fe23ba2ed1e7aa63c8ec6fb13add70570af15c65
Emmanuel <emmanuelhemarxyll@gmail.com>
2022-11-11 11:42:15 +0100

    setup baseweb for improved styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,10 @@ import PropTypes from 'prop-types'
 import { useEffect } from 'react'
 import { Provider } from 'react-redux'
 import store from '../../redux/store'
+import { Provider as StyletronProvider } from "styletron-react";
+import { styletron } from '../styletron';
+import { BaseProvider } from "baseui";
+import { theme } from "../theme";
 
 function MyApp({ Component, pageProps: { ...pageProps } }: any) {
   const [state, setState] = useState(false)
@@ -14,15 +18,16 @@ function MyApp({ Component, pageProps: { ...pageProps } }: any) {
     require('@fortawesome/fontawesome-free/js/all.js')
     setState(true)
   }, []);
-
   return (
-    <>
-      <Provider store={store}>
-        {state ?
-          <Component {...pageProps} />
-          : null}
-      </Provider>
-    </>
+    <StyletronProvider value={styletron}>
+      <BaseProvider theme={theme}>
+        <Provider store={store}>
+          {state ?
+            <Component {...pageProps} />
+            : null}
+        </Provider>
+      </BaseProvider>
+    </StyletronProvider>
   )
 }
 MyApp.propTypes = {
